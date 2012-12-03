db147388d835b65e9f94bb6e98a9c32a7edff8e7
Nicolas Kijak <nkijak@gmail.com>
2012-12-02 23:39:34 -0500

    Added UUID cookie to track individual clients

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-Demo of distributed prime number calculation.  To compute more numbers faster just open up the index page in more browsers.
+Demo of distributed prime number calculation.  To compute more numbers faster just open up the index page in more browsers. Requires redis-server to be installed and running.
+
+* sudo apt-get install redis-server node npm
+
+(node probably wont be the right version on ubuntu google for it.)
 
 * npm install
 * node server
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ function CUTOFF_VALUE() { return 1000000; }
 var express = require('express'),
     path    = require('path'),
 	cons    = require('consolidate'),
-	redis 	= require('redis').createClient();
+	redis 	= require('redis').createClient(),
+	uuid	= require('node-uuid');
 var app = express();
 
 app.configure(function(){
@@ -14,6 +15,16 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(function(req, res, next) {
+	var userId = req.cookies.uuid;
+	if (!userId) {
+		userId = uuid.v1();
+    	res.cookie('uuid', userId,{maxAge: 90000000}); 
+	}
+	req.userId = userId;
+	next();
+  });
   app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
@@ -24,16 +35,18 @@ app.get('/', function(req, res) {
 });
 
 app.get('/number', function(req, res) {
-    redis.incr('current-number', function(err, currentNumber) {
-		if (currentNumber > CUTOFF_VALUE()) {
-			res.writeHead(503);
-			res.end();
-		} else {
-			var object = {'number':currentNumber};
-			var body = JSON.stringify(object);
-			res.setHeader('Content-Type', 'application/json');
-			res.send(body);
-		}
+    redis.incr('current-number', function(err1, currentNumber) {
+		redis.hset('user-numbers', req.userId, currentNumber, function(err2, reply) {
+			if (currentNumber > CUTOFF_VALUE()) {
+				res.writeHead(503);
+				res.end();
+			} else {
+				var object = {'number':currentNumber};
+				var body = JSON.stringify(object);
+				res.setHeader('Content-Type', 'application/json');
+				res.send(body);
+			}
+		});
     });
 });
 
@@ -43,11 +56,15 @@ app.get('/stats', function(req, res) {
 });
 
 app.post('/number', function(req, res) {
-	if (req.body.isPrime) {
-		redis.zadd("prime-number", new Date().getTime(), req.body.number);
-	}
-	res.writeHead(204);
-	res.end();
+	redis.hdel('user-numbers', req.userId, function(err3, count) {
+		redis.zadd(req.userId+"-processed", Date.now(), req.body.number);
+		if (req.body.isPrime) {
+			redis.zadd("prime-number", Date.now(), req.body.number);
+			redis.zadd(req.userId+"-primes", Date.now(), req.body.number);
+		}
+		res.writeHead(204);
+		res.end();
+	});
 });
 
 app.listen(3010);
